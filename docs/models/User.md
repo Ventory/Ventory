00f0e277b4f79ea0722b8f44a05bb1f2b3517df4
Ben Ludewig <ludewig.b@gmail.com>
2015-04-03 17:10:04 +0200

    User Model + npm

---
+++ docs/models/User.md
@@ -0,0 +1,16 @@
+User Model
+
+<table>
+	<tr>
+		<th>Property</th>
+		<th>Description</th>
+		<th>Type</th>
+		<th>Default</th>
+	</tr>
+	<tr>
+		<td>createdAt</td>
+		<td></td>
+		<td>Date<</td>
+		<td>Date.now</td>
+	</tr>
+</table>
--- models/users.js
+++ models/users.js
@@ -1,2 +1,43 @@
 var mongoose = require('mongoose');
-var model = mongoose.Model;
+var Schema = mongoose.Schema;
+
+var usersSchema = new Schema({
+	// Admin Info
+	createdAt: { type: Date, default: Date.now },
+	type: { type: Number, default: 0 },
+	passwordHash: { type: String, required: true },
+	// Status
+	suspended: { type: Number, default: 0 },
+	ipHistory: { type: Array, default: [] },
+	searchHistory: { type: Array, default: [] },
+	categoryHistory: { type: Array, default: [] },
+	// Profile Info
+	email: { type: String, required: true, trim: true},
+	name: { type: String, required: true, trim: true},
+	firstname: { type: String, required: true, trim: true},
+	gender: { type: Number, required: true},
+	language: { type: String, required: true},
+	settings: { type: Array, default: [] },
+	picture: { type: String },
+	pictureThumbnail: { type: String},
+	address: { type: String },
+	country: { type: String },
+	// Market
+	companyName: { type: String },
+	orders: [ordersSchema],
+	subusers: { type: Array, default: [] }
+});
+
+usersSchema.methods.fullName = function() {
+	var fullName = this.vname + ' ' + this.nname;
+	return fullName;
+}
+
+usersSchema.methods.validatePassword = function(pwd, callback) {
+    var hash = this.passwordHash;
+    bcrypt.compare(pwd, hash, function(err, res) {
+    	return callback(res);
+	});
+};
+
+mongoose.model('users', usersSchema);
