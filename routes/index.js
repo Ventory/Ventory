ca76097de1db6b33fd5dbc05d474a24e4beb6888
Ventory <mail@ventory.de>
2015-04-03 18:19:02 +0200

    Partial Setup

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+
+var partialSource = {
+	standard : {
+		partHeader: '../views/partials/part-header',
+  		partFooter: '../views/partials/part-footer' 
+	}
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
   	title: 'Express',
-  	partials: {
-  		partHeader: '../views/partials/part-header',
-  		partFooter: '../views/partials/part-footer'
-  	}
+  	partials: partialSource.standard
   });
 });
 
