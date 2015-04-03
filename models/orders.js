0fc53569e8e38d1297844a2c1cba1537572c1781
Ben Ludewig <ludewig.b@gmail.com>
2015-04-03 17:29:35 +0200

    Models + Docs

---
+++ models/orders.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var orderSchema = new Schema({
+
+});
+
+mongoose.model("order", orderSchema);
--- models/users.js
+++ models/users.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
+var orderSchema = require('./orders')
 var Schema = mongoose.Schema;
 
-var usersSchema = new Schema({
+var userSchema = new Schema({
 	// Admin Info
 	createdAt: { type: Date, default: Date.now },
 	type: { type: Number, default: 0 },
@@ -10,34 +11,30 @@ var usersSchema = new Schema({
 	suspended: { type: Number, default: 0 },
 	ipHistory: { type: Array, default: [] },
 	searchHistory: { type: Array, default: [] },
-	categoryHistory: { type: Array, default: [] },
 	// Profile Info
 	email: { type: String, required: true, trim: true},
 	name: { type: String, required: true, trim: true},
 	firstname: { type: String, required: true, trim: true},
 	gender: { type: Number, required: true},
-	language: { type: String, required: true},
 	settings: { type: Array, default: [] },
 	picture: { type: String },
 	pictureThumbnail: { type: String},
 	address: { type: String },
 	country: { type: String },
 	// Market
-	companyName: { type: String },
-	orders: [ordersSchema],
-	subusers: { type: Array, default: [] }
+	orders: [orderSchema],
 });
 
-usersSchema.methods.fullName = function() {
+userSchema.methods.fullName = function() {
 	var fullName = this.vname + ' ' + this.nname;
 	return fullName;
 }
 
-usersSchema.methods.validatePassword = function(pwd, callback) {
+userSchema.methods.validatePassword = function(pwd, callback) {
     var hash = this.passwordHash;
     bcrypt.compare(pwd, hash, function(err, res) {
     	return callback(res);
 	});
 };
 
-mongoose.model('users', usersSchema);
+mongoose.model('user', userSchema);
