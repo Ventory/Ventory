d2a7fcc51f6f8999cb1de8ee6affafeb5264dfd0
Steffen Jäckel <derjaeckel@googlemail.com>
2015-04-03 19:29:04 +0200

    Builded maybe a nice template hierachival structure

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.render('users/index', {
+        title : "This is the title for users",
+        content : "This is content users"
+    });
 });
 
 module.exports = router;
