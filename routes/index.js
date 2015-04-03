f2f85bf4cba0d3788a72a950835b023fa549186c
Ventory <mail@ventory.de>
2015-04-03 18:11:17 +0200

    Split Footer and Header into Partials

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+  	title: 'Express',
+  	partials: {
+  		partHeader: '../views/partials/part-header',
+  		partFooter: '../views/partials/part-footer'
+  	}
+  });
 });
 
 module.exports = router;
