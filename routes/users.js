9d680f2467cd4cff9ad813646f67a527984b6449
Steffen Jäckel <derjaeckel@googlemail.com>
2015-04-07 16:40:55 +0200

    Testing the translation by using the request-object of the get-index-request call

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,8 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res) {
     res.render('users/index', {
-        title : "This is the title for users",
-        content : "This is content users"
+        title : res.__("This is the %s title for users", res.__("awesome")),
+        content : res.__("This is content users")
     });
 });
 
