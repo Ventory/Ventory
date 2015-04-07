d4c755f92c9e9aec179fab531e299ead2a6a39d4
Ventory <mail@ventory.de>
2015-04-07 15:26:09 +0200

    Title Update

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', {
-        title : "This is the title",
+        title : "Ventory",
         content : "This is the content"
     });
 });
