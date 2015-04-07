ae496798d67f1ff8e604a7a81723caf7243d058b
Ben Ludewig <ludewig.b@gmail.com>
2015-04-07 16:00:36 +0200

    Signup code

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var userc = require('../controller/users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,8 +12,20 @@ router.get('/', function(req, res, next) {
 
 router.get('/signup', function(req, res) {
 	res.render('users/signup', {
-		title: "Ventory - Sign Up"
+		title: "Ventory - Sign Up",
+		flashMessage: req.flash('loginError')
 	})
+});
+
+router.post('/signup', function(req, res) {
+	userc.addUser(req.body, function(err){
+		if (err) {
+			req.flash('loginError', err);
+			res.redirect('/signup');
+		} else {
+			res.redirect('/signupsuccess')
+		}
+	});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var userc = require('../controller/users');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.render('users/index', {
         title : "This is the title for users",
         content : "This is content users"
     });
+});
+
+router.post('/search.:format?', function(req, res) {
+
 });
 
 module.exports = router;
