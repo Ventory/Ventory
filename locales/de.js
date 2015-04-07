78f5313451dad58fc516fa4995e9005b88b3b83f
Steffen Jäckel <derjaeckel@googlemail.com>
2015-04-07 16:38:56 +0200

    Translated some english test strings

--- locales/de.js
+++ locales/de.js
@@ -1,1 +1,6 @@
-{}
+{
+	"awesome": "unglaublicher",
+	"This is the %s title for users": "Dies ist ein %s Titel für den Benutzer-Controller",
+	"This is content users": "Dies soll der Content sein",
+	"This is a translationtext": "Dies ein Translationtext"
+}
