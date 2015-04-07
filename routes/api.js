d5644e50ad1ab9717b9d9df19dff88cd24682762
Ben Ludewig <ludewig.b@gmail.com>
2015-04-07 17:12:29 +0200

    API changes

--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,7 @@ var userc = require('../controller/users')
 
 router.post('/signup', function(req, res) {
 	userc.addUser(req.body, function(result){
-		res.send(JSON.stringify(result));
-		res.close();
+		res.send(result);
 	});
 });
 
